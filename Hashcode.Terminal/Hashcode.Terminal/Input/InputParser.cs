b3714b50d9182ea8f15b7e4bba3f04c78f3b253b
Evert De Vos <Evert.DeVos@realdolmen.com>
2018-03-01 19:14:15 +0100

    added input parser and values map

---
+++ Hashcode.Terminal/Hashcode.Terminal/Input/InputParser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Hashcode.Terminal.Models;
+
+namespace Hashcode.Terminal
+{
+    public class InputParser
+    {
+
+        public static ValuesMap ParseInput(string path)
+        {
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+           // read first line
+            var values = lines[0].Split(' ');
+            var rows = values[0];
+            var columns = values[1];
+            var vihicules = values[2];
+            var rides = values[3];
+            var bonus = values[4];
+            var steps = values[5];
+            
+            foreach (string line in lines)
+            {
+               
+            }
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ Hashcode.Terminal/Hashcode.Terminal/Models/ValuesMap.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hashcode.Terminal.Models
+{
+    public class ValuesMap
+    {
+    }
+}
