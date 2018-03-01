2d8481734428776056a452182f6ed4a0e6c3b559
Glenn Van Looveren <glenn.vanlooveren@realdolmen.com>
2018-03-01 19:54:26 +0100

    cars

--- Hashcode.Terminal/Hashcode.Terminal/Models/Ride.cs
+++ Hashcode.Terminal/Hashcode.Terminal/Models/Ride.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Hashcode.Terminal.Models
 {
-    class Ride
+    public class Ride
     {
+        public Point Start { get; set; }
+        public Point End { get; set; }
+        public int startTick { get; set; }
+        public int EndTick { get; set; }
+
+        public int GetDuration()
+        {
+            return 0;
+        }
     }
 }
