aae11705bd98dc3861cf1b3f3a714cde5e7e69e9
Evert De Vos <Evert.DeVos@realdolmen.com>
2018-03-01 19:24:29 +0100

    adde point struct

---
+++ Hashcode.Terminal/Hashcode.Terminal/Models/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hashcode.Terminal.Models
+{
+    public struct Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
